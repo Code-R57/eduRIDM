6425651e63da98353d4535811005665287ff9dfb
Ritvik <CodingR57@gmail.com>
2022-11-21 02:18:25 +0530

    Add NavGraph

---
+++ app/src/main/java/com/ridm/eduRIDM/screen/AddPlan/AddPlanFragment.java
@@ -0,0 +1,24 @@
+package com.ridm.eduRIDM.screen.AddPlan;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.ridm.eduRIDM.R;
+
+public class AddPlanFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_plan, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/ridm/eduRIDM/screen/TimetableEdit/AddTimetableFragment.java
@@ -0,0 +1,24 @@
+package com.ridm.eduRIDM.screen.TimetableEdit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.ridm.eduRIDM.R;
+
+public class AddTimetableFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_timetable, container, false);
+        return view;
+    }
+}
