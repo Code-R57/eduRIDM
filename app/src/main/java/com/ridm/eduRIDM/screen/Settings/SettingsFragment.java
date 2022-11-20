b245c04ad06030f9b2cfdfbee65e8cfb10e43da7
Ritvik <74790215+Code-R57@users.noreply.github.com>
2022-11-21 02:18:58 +0530

    Add NavGraph

---
+++ app/src/main/java/com/ridm/eduRIDM/screen/Settings/SettingsFragment.java
@@ -0,0 +1,24 @@
+package com.ridm.eduRIDM.screen.Settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.ridm.eduRIDM.R;
+
+public class SettingsFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+}
