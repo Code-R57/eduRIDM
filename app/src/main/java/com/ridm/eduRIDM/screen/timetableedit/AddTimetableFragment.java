d85037c576a557a68224ecd22d2cbab10ca05ad3
DarshanChandak <chandak.darshan0@gmail.com>
2022-11-22 03:38:03 +0530

    Add Navigation from Add Timetable to Home Screen Layout

--- app/src/main/java/com/ridm/eduRIDM/screen/timetableedit/AddTimetableFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/timetableedit/AddTimetableFragment.java
@@ -6,11 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.ridm.eduRIDM.R;
+import com.ridm.eduRIDM.databinding.FragmentAddTimetableBinding;
+import com.ridm.eduRIDM.databinding.FragmentEditTimetableBinding;
+import com.ridm.eduRIDM.databinding.FragmentPlannerBinding;
+import com.ridm.eduRIDM.screen.planner.PlannerViewModel;
 
 public class AddTimetableFragment extends Fragment {
+
+    AddTimetableViewModel viewModel;
+    FragmentAddTimetableBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +29,21 @@ public class AddTimetableFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_timetable, container, false);
-        return view;
+        viewModel = new ViewModelProvider(this).get(AddTimetableViewModel.class);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_timetable, container, false);
+
+        binding.setLifecycleOwner(this);
+
+        binding.setViewModel(viewModel);
+
+        viewModel.getNavigateToHomeScreen().observe(getViewLifecycleOwner(), navigateToHomeScreen -> {
+            if(navigateToHomeScreen == Boolean.TRUE) {
+                Navigation.findNavController(this.requireView()).navigate(R.id.action_addTimetableFragment_to_homeScreenFragment);
+                viewModel.doneReachingHomeScreen();
+            }
+        });
+
+        return binding.getRoot();
     }
 }
