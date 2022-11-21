75801dd2bcf020ec14b5adf51a6dcb88b29e953c
Ritvik <CodingR57@gmail.com>
2022-11-22 02:27:40 +0530

    Add Navigation for MyAcads

--- app/src/main/java/com/ridm/eduRIDM/screen/myacads/MyAcadsFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/myacads/MyAcadsFragment.java
@@ -6,11 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.ridm.eduRIDM.R;
+import com.ridm.eduRIDM.databinding.FragmentMyAcadsBinding;
+import com.ridm.eduRIDM.screen.planner.PlannerViewModel;
 
 public class MyAcadsFragment extends Fragment {
+
+    MyAcadsViewModel viewModel;
+    FragmentMyAcadsBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +27,22 @@ public class MyAcadsFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_acads, container, false);
-        return view;
+
+        viewModel = new ViewModelProvider(this).get(MyAcadsViewModel.class);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_acads, container, false);
+
+        binding.setLifecycleOwner(this);
+
+        binding.setViewModel(viewModel);
+
+        viewModel.getNavigateToAddEval().observe(getViewLifecycleOwner(), navigateToAddEval -> {
+            if(navigateToAddEval == Boolean.TRUE) {
+                Navigation.findNavController(this.requireView()).navigate(R.id.action_myAcadsFragment_to_addEvaluativeFragment);
+                viewModel.doneNavigatingToAddEval();
+            }
+        });
+
+        return binding.getRoot();
     }
 }
