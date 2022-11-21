b1fd06677a46bcdc0e66433a218313638b99404c
Ritvik <CodingR57@gmail.com>
2022-11-22 01:58:57 +0530

    Add Navigation to Planner

--- app/src/main/java/com/ridm/eduRIDM/screen/addplan/AddPlanFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/addplan/AddPlanFragment.java
@@ -6,11 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.ridm.eduRIDM.R;
+import com.ridm.eduRIDM.databinding.FragmentAddPlanBinding;
 
 public class AddPlanFragment extends Fragment {
+
+    AddPlanViewModel viewModel;
+    FragmentAddPlanBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +26,22 @@ public class AddPlanFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_plan, container, false);
-        return view;
+
+        viewModel = new ViewModelProvider(this).get(AddPlanViewModel.class);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_plan, container, false);
+
+        binding.setLifecycleOwner(this);
+
+        binding.setViewModel(viewModel);
+
+        viewModel.getNavigateToPlanner().observe(getViewLifecycleOwner(), navigateToPlanner -> {
+            if(navigateToPlanner == Boolean.TRUE) {
+                Navigation.findNavController(this.requireView()).navigate(R.id.action_addPlanFragment_to_plannerFragment);
+                viewModel.doneNavigatingToPlanner();
+            }
+        });
+
+        return binding.getRoot();
     }
 }
