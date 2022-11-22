de830fd2b338ed59eb38c64bcea40aacc9f40deb
Mufaddal24 <87411687+Mufaddal24@users.noreply.github.com>
2022-11-22 23:40:14 +0530

    Implementation of Bottom Navigation

--- app/src/main/java/com/ridm/eduRIDM/MainActivity.java
+++ app/src/main/java/com/ridm/eduRIDM/MainActivity.java
@@ -1,16 +1,23 @@
 package com.ridm.eduRIDM;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ridm.eduRIDM.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    public BottomNavigationView bottomView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+        bottomView = findViewById(R.id.bottomNav);
+        NavigationUI.setupWithNavController(bottomView, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                if(navDestination.getId() == R.id.homeScreenFragment || navDestination.getId() == R.id.plannerFragment || navDestination.getId() == R.id.myAcadsFragment) {
+                    bottomView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    bottomView.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
