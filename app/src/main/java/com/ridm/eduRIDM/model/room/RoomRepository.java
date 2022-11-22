404d1256e18bda1271c2c7018e6aebec8ce7605c
Ec5tacy <94638670+Ec5tacy@users.noreply.github.com>
2022-11-23 00:14:45 +0530

    Add room database connections for addExtraFragment , AddEvaluativeFragment (#23)
    Update RoomRepository
    Update AddEvalViewModel , AddExtraclassViewModel
    
    Add string-array resource for subject and time
    add Dummy entries for subject and time in fragment_add_eval, fragment_add_extra_class layout

--- app/src/main/java/com/ridm/eduRIDM/model/room/RoomRepository.java
+++ app/src/main/java/com/ridm/eduRIDM/model/room/RoomRepository.java
@@ -2,6 +2,8 @@ package com.ridm.eduRIDM.model.room;
 
 import android.app.Application;
 
+import com.ridm.eduRIDM.model.room.Eval.Eval;
+import com.ridm.eduRIDM.model.room.ExtraClass.ExtraClass;
 import com.ridm.eduRIDM.model.room.CurrentGrade.CurrentGrade;
 import com.ridm.eduRIDM.model.room.Plan.Plan;
 import com.ridm.eduRIDM.model.room.Plan.PlanDao;
@@ -25,6 +27,24 @@ public class RoomRepository {
             @Override
             public void run() {
                 appDatabase.planDao().insertPlan(plan);
+            }
+        });
+    }
+
+    public void insertEval(Eval eval){
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                appDatabase.evalDao().insertEval(eval);
+            }
+        });
+    }
+    
+    public void insertExtraClass(ExtraClass extraClass){
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                appDatabase.extraClassDao().insertExtraClass(extraClass);
             }
         });
     }
