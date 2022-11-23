d2eb7737dcc542f35b73f236015e1996e138de3a
DarshanChandak <chandak.darshan0@gmail.com>
2022-11-23 15:49:31 +0530

    Fixed Toolbar Items visibilty in different layouts

--- app/src/main/java/com/ridm/eduRIDM/screen/onboarding/RegisterFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/onboarding/RegisterFragment.java
@@ -2,11 +2,14 @@ package com.ridm.eduRIDM.screen.onboarding;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
