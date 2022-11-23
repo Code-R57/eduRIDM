d4ff634099ccbfb57b6464aa1a591ac7b57279f8
Ritvik <74790215+Code-R57@users.noreply.github.com>
2022-11-23 21:03:14 +0530

    Add RecyclerView for MyAcads (#30)
    * Add RecyclerView for MyAcads

--- app/src/main/java/com/ridm/eduRIDM/screen/myacads/MyAcadsFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/myacads/MyAcadsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +12,11 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.ridm.eduRIDM.R;
 import com.ridm.eduRIDM.databinding.FragmentMyAcadsBinding;
+import com.ridm.eduRIDM.model.room.TimeTable.TimeTable;
 import com.ridm.eduRIDM.screen.planner.PlannerViewModel;
 
 public class MyAcadsFragment extends Fragment {
@@ -24,6 +27,15 @@ public class MyAcadsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(MyAcadsViewModel.class);
+
+        viewModel.getAllCourses();
+        viewModel.getAllEvals();
+
+        for(TimeTable course: viewModel.courses) {
+            viewModel.getBacklogForCourse(course.getDeptCode(), course.getCourseCode(), course.getCourseName());
+        }
     }
 
     @Override
@@ -41,6 +53,24 @@ public class MyAcadsFragment extends Fragment {
             if(navigateToAddEval == Boolean.TRUE) {
                 Navigation.findNavController(this.requireView()).navigate(R.id.action_myAcadsFragment_to_addEvaluativeFragment);
                 viewModel.doneNavigatingToAddEval();
+            }
+        });
+
+        viewModel.getCurrentSelection().observe(getViewLifecycleOwner(), checkedState -> {
+            AcadsListAdapter acadsListAdapter = new AcadsListAdapter(requireContext(), viewModel.courses, viewModel.evalList, checkedState, viewModel.courseBacklog);
+            binding.acadsList.setAdapter(acadsListAdapter);
+//            binding.acadsList.setLayoutManager(new LinearLayoutManager(getContext()));
+        });
+
+        binding.evalBacklogSelector.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(binding.evalBacklogSelector.getCheckedRadioButtonId() == R.id.evals) {
+                    viewModel.setCurrentSelection("Evals");
+                }
+                else {
+                    viewModel.setCurrentSelection("Backlog");
+                }
             }
         });
 
