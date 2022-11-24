76800bd4fe7e2f3bcf54784263b938d48f42138f
Ritvik <CodingR57@gmail.com>
2022-11-24 19:48:53 +0530

    Add getCourses Query

--- app/src/main/java/com/ridm/eduRIDM/model/firebase/FirebaseQueries.java
+++ app/src/main/java/com/ridm/eduRIDM/model/firebase/FirebaseQueries.java
@@ -6,11 +6,19 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.ridm.eduRIDM.MainActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class FirebaseQueries {
@@ -38,5 +46,27 @@ public class FirebaseQueries {
                         Toast.makeText(context, "Registration Failed", Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    public List<QueryDocumentSnapshot> getCourses() {
+        CollectionReference collectionReference = database.collection("courses");
+
+        List<QueryDocumentSnapshot> coursesList = new ArrayList<>();
+
+        collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for(QueryDocumentSnapshot document: task.getResult()) {
+                        coursesList.add(document);
+                    }
+                }
+                else {
+                    Toast.makeText(context, "Unable to get courses.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return coursesList;
     }
 }
--- app/src/main/java/com/ridm/eduRIDM/screen/addevaluative/AddEvaluativeFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/addevaluative/AddEvaluativeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -11,10 +12,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.ridm.eduRIDM.R;
 import com.ridm.eduRIDM.databinding.FragmentAddEvaluativeBinding;
 import com.ridm.eduRIDM.databinding.FragmentAddEvaluativeBindingImpl;
 import com.ridm.eduRIDM.model.room.Eval.Eval;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddEvaluativeFragment extends Fragment {
 
@@ -24,12 +29,14 @@ public class AddEvaluativeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(AddEvaluativeViewModel.class);
+
+        viewModel.getMyCourses();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        viewModel = new ViewModelProvider(this).get(AddEvaluativeViewModel.class);
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_evaluative, container, false);
 
