181662d71a78ed486b0da31f6a20c5411c7e2402
DarshanChandak <chandak.darshan0@gmail.com>
2022-11-24 19:44:50 +0530

    Changed Spinners to DatePicker and TimePicker

--- app/src/main/java/com/ridm/eduRIDM/screen/planner/PlannerFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/planner/PlannerFragment.java
@@ -1,9 +1,11 @@
 package com.ridm.eduRIDM.screen.planner;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -16,12 +18,19 @@ import com.ridm.eduRIDM.R;
 import com.ridm.eduRIDM.databinding.FragmentPlannerBinding;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class PlannerFragment extends Fragment {
 
     PlannerViewModel viewModel;
     FragmentPlannerBinding binding;
+
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,6 +62,25 @@ public class PlannerFragment extends Fragment {
         PlanCardAdapter adapter = new PlanCardAdapter(requireContext(), viewModel.planList);
         binding.recyclerView.setAdapter(adapter);
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        binding.plannerDateSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                binding.plannerDateSelector.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
 
         return binding.getRoot();
     }
