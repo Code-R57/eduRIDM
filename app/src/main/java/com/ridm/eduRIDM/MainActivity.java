c160efe014c1d28176aef9ea05ca063c9048d8eb
Ritvik <CodingR57@gmail.com>
2022-11-24 05:37:54 +0530

    Fix Navigation in Google Sign In

--- app/src/main/java/com/ridm/eduRIDM/MainActivity.java
+++ app/src/main/java/com/ridm/eduRIDM/MainActivity.java
@@ -1,5 +1,13 @@
 package com.ridm.eduRIDM;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,53 +18,40 @@ import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.ridm.eduRIDM.databinding.ActivityMainBinding;
 import com.ridm.eduRIDM.model.room.RoomRepository;
+import com.ridm.eduRIDM.screen.onboarding.WelcomeScreenFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WelcomeScreenFragment.Listener {
 
+    public static RoomRepository roomRepository;
+    public static GoogleSignInClient mGoogleSignInClient;
+    public static int RC_SIGN_IN = 57;
+    public static FirebaseAuth mAuth;
+    public static GoogleSignInAccount account;
     public BottomNavigationView bottomView;
     public Toolbar toolbar;
     NavController navController;
 
-    public static RoomRepository roomRepository;
-
-    public static GoogleSignInClient mGoogleSignInClient;
-    public FirebaseAuth mAuth;
-    public GoogleSignInAccount account;
-    public static int RC_SIGN_IN = 57;
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == RC_SIGN_IN) {
+        if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
             try {
                 account = task.getResult(ApiException.class);
-                navController.navigate(R.id.registerFragment);
+                firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
                 Toast.makeText(this, "Google Sign In Failed", Toast.LENGTH_SHORT).show();
             }
@@ -69,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         account = GoogleSignIn.getLastSignedInAccount(this);
 
-        if(account != null) {
+        if (account != null) {
             navController.navigate(R.id.homeScreenFragment);
         }
     }
@@ -81,8 +76,7 @@ public class MainActivity extends AppCompatActivity {
         // Google Sign In
         mAuth = FirebaseAuth.getInstance();
 
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail().build();
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
@@ -161,58 +155,49 @@ public class MainActivity extends AppCompatActivity {
             public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
                 if (navDestination.getId() == R.id.stopwatchScreenFragment || navDestination.getId() == R.id.registerFragment || navDestination.getId() == R.id.addTimetableFragment) {
                     MenuItem item1 = menu.findItem(R.id.settingsFragment);
-                    if (item1 != null)
-                        item1.setVisible(false);
+                    if (item1 != null) item1.setVisible(false);
 
                     MenuItem item2 = menu.findItem(R.id.profileScreenFragment);
-                    if (item2 != null)
-                        item2.setVisible(false);
+                    if (item2 != null) item2.setVisible(false);
                 } else if (navDestination.getId() == R.id.profileScreenFragment) {
                     MenuItem item1 = menu.findItem(R.id.settingsFragment);
-                    if (item1 != null)
-                        item1.setVisible(true);
+                    if (item1 != null) item1.setVisible(true);
 
                     MenuItem item2 = menu.findItem(R.id.profileScreenFragment);
-                    if (item2 != null)
-                        item2.setVisible(false);
+                    if (item2 != null) item2.setVisible(false);
                 } else if (navDestination.getId() == R.id.settingsFragment) {
                     MenuItem item1 = menu.findItem(R.id.settingsFragment);
-                    if (item1 != null)
-                        item1.setVisible(false);
+                    if (item1 != null) item1.setVisible(false);
 
                     MenuItem item2 = menu.findItem(R.id.profileScreenFragment);
-                    if (item2 != null)
-                        item2.setVisible(true);
-                }
-                else
-                {
+                    if (item2 != null) item2.setVisible(true);
+                } else {
                     MenuItem item1 = menu.findItem(R.id.settingsFragment);
-                    if (item1 != null)
-                        item1.setVisible(true);
+                    if (item1 != null) item1.setVisible(true);
 
                     MenuItem item2 = menu.findItem(R.id.profileScreenFragment);
-                    if (item2 != null)
-                        item2.setVisible(true);
+                    if (item2 != null) item2.setVisible(true);
                 }
             }
         });
         return true;
     }
 
-    public void getFirebaseAuthWithGoogle() {
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-
-        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                boolean isNewUser = task.getResult().getAdditionalUserInfo().isNewUser();
-                if (isNewUser) {
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    navController.navigate(R.id.registerFragment);
-                } else {
-                    navController.navigate(R.id.homeScreenFragment);
-                }
+        mAuth.signInWithCredential(credential).addOnSuccessListener(this, authResult -> {
+            boolean isNewUser = authResult.getAdditionalUserInfo().isNewUser();
+            if (isNewUser) {
+                navController.navigate(R.id.registerFragment);
+            } else {
+                navController.navigate(R.id.homeScreenFragment);
             }
-        });
+        }).addOnFailureListener(this, e -> Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    public void signInClicked() {
+        Intent intent = MainActivity.mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, MainActivity.RC_SIGN_IN);
     }
 }
--- app/src/main/java/com/ridm/eduRIDM/screen/onboarding/WelcomeScreenFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/onboarding/WelcomeScreenFragment.java
@@ -1,22 +1,17 @@
 package com.ridm.eduRIDM.screen.onboarding;
 
-import static androidx.core.app.ActivityCompat.startActivityForResult;
-
-import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
-import com.ridm.eduRIDM.MainActivity;
 import com.ridm.eduRIDM.R;
 import com.ridm.eduRIDM.databinding.FragmentWelcomeScreenBinding;
 
@@ -24,7 +19,13 @@ public class WelcomeScreenFragment extends Fragment {
 
     WelcomeScreenViewModel viewModel;
     FragmentWelcomeScreenBinding binding;
+    private Listener listener;
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,14 +37,14 @@ public class WelcomeScreenFragment extends Fragment {
 
         viewModel = new ViewModelProvider(this).get(WelcomeScreenViewModel.class);
 
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_welcome_screen, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome_screen, container, false);
 
         binding.setLifecycleOwner(this);
 
         binding.setViewModel(viewModel);
 
         viewModel.getNavigateToRegister().observe(getViewLifecycleOwner(), navigateToRegister -> {
-            if(navigateToRegister == Boolean.TRUE) {
+            if (navigateToRegister == Boolean.TRUE) {
                 Navigation.findNavController(this.requireView()).navigate(R.id.action_welcomeScreenFragment_to_registerFragment);
                 viewModel.doneNavigatingToRegister();
             }
@@ -52,11 +53,14 @@ public class WelcomeScreenFragment extends Fragment {
         binding.googleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = MainActivity.mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(intent, MainActivity.RC_SIGN_IN);
+                listener.signInClicked();
             }
         });
 
         return binding.getRoot();
+    }
+
+    public interface Listener {
+        void signInClicked();
     }
 }
