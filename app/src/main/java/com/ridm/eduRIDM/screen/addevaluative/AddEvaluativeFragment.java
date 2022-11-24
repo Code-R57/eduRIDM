b0b3296b1f75d7ea51f605b1f82037a5e8f4293d
DarshanChandak <93312356+DarshanChandak@users.noreply.github.com>
2022-11-24 19:51:25 +0530

    Changed Spinners to DatePicker and TimePicker (#37)
    * Add Toolbar
    
    * Changed Spinners to DatePicker and TimePicker

--- app/src/main/java/com/ridm/eduRIDM/screen/addevaluative/AddEvaluativeFragment.java
+++ app/src/main/java/com/ridm/eduRIDM/screen/addevaluative/AddEvaluativeFragment.java
@@ -1,9 +1,14 @@
 package com.ridm.eduRIDM.screen.addevaluative;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
@@ -12,12 +17,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.ridm.eduRIDM.MainActivity;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.ridm.eduRIDM.R;
 import com.ridm.eduRIDM.databinding.FragmentAddEvaluativeBinding;
 import com.ridm.eduRIDM.databinding.FragmentAddEvaluativeBindingImpl;
 import com.ridm.eduRIDM.model.room.Eval.Eval;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +34,13 @@ public class AddEvaluativeFragment extends Fragment {
 
     AddEvaluativeViewModel viewModel;
     FragmentAddEvaluativeBinding binding;
+
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,22 +72,59 @@ public class AddEvaluativeFragment extends Fragment {
             public void onClick(View v) {
                 Eval eval = new Eval();
 
-                String course[]=binding.spinnerSubject.getSelectedItem().toString().split(" ");
+                String course[] = binding.spinnerSubject.getSelectedItem().toString().split(" ");
 
                 eval.setDeptCode(course[0]);
-                eval.setCourseCode(course[1]);
-                eval.setDate(binding.spinnerDate.getSelectedItem().toString());
-                eval.setTime(binding.spinnerTime.getSelectedItem().toString());
-                eval.setDuration(binding.spinnerDuration.getSelectedItemPosition());
+//                eval.setCourseCode(course[1]);
+                eval.setDate(binding.addEvalDatePicker.getText().toString());
+                eval.setTime(binding.addEvalTimePicker.getText().toString());
+                eval.setDuration(Integer.parseInt(binding.addEvalDuration.getText().toString()));
                 eval.setType(binding.spinnerType.getSelectedItem().toString());
                 eval.setSyllabus(binding.syllabusText.getText().toString());
                 eval.setNature(binding.spinnerNature.getSelectedItem().toString());
 
-                viewModel.eval=eval;
+                viewModel.eval = eval;
                 viewModel.onNavigateToMyAcadsClicked();
             }
         });
 
+        binding.addEvalDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                binding.addEvalDatePicker.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        binding.addEvalTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        String amPm;
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        binding.addEvalTimePicker.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                    }
+                }, 0, 0, false);
+                timePickerDialog.show();
+            }
+        });
         return binding.getRoot();
     }
 }
